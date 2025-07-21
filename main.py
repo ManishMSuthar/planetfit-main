f7d15cec19c221c489bf3ec1c9877d98b00f0ae3
ManishMSuthar <manishmukeshsuthar@gmail.com>
2025-07-21 23:15:01 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class PlanetfitApp(App):
-    def build(self):
-        return Label(text="Welcome to PlanetFit!")
-
-if __name__ == '__main__':
-    PlanetfitApp().run()
